c0cd0ec58b64fe0c091fa4b26a0f8ef4a4166524
Lucas Perna <lucasmalamao@gmail.com>
2020-08-18 22:50:35 -0300

    Validacao sintatica utilizando Bean Validation

--- src/main/java/com/giga/cursomc/services/CategoriaService.java
+++ src/main/java/com/giga/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.giga.cursomc.domain.Categoria;
+import com.giga.cursomc.dto.CategoriaDTO;
 import com.giga.cursomc.respositories.CategoriaRepository;
 import com.giga.cursomc.services.exceptions.DataIntegrityException;
 import com.giga.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
