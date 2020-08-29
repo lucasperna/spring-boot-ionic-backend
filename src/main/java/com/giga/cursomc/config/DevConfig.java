5db413126d8fe2324c1f5b706ed622164c45bd1c
Lucas Perna <lucasmalamao@gmail.com>
2020-08-29 14:02:16 -0300

    Criado envio de e-mail pelo servidor Google

--- src/main/java/com/giga/cursomc/config/DevConfig.java
+++ src/main/java/com/giga/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.giga.cursomc.services.DBService;
+import com.giga.cursomc.services.EmailService;
+import com.giga.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,5 +29,10 @@ public class DevConfig {
 		}
 		dbService.instateateDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
