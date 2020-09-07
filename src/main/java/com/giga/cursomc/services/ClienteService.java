86ec8ebd0cb7a10aa42be4fcc3422c131333b650
Lucas Perna <lucasmalamao@gmail.com>
2020-09-07 13:16:54 -0300

    URL do profile image do Cliente salva no BD

--- src/main/java/com/giga/cursomc/services/ClienteService.java
+++ src/main/java/com/giga/cursomc/services/ClienteService.java
@@ -111,6 +111,15 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		UserSS user = UserService.authenticated();
+		if(user == null )	{
+			throw new AuthorizationException("Acesso negado!");
+		}
+		URI uri = s3Service.uploadFile(multipartFile);
+		
+		Cliente cli = find(user.getId());
+		cli.setImageURL(uri.toString());
+		repo.save(cli);
+		return uri;
 	}
 }
