fa6dea1aa8d7cb5d4070892c89aa554fa43747e7
Lucas Perna <lucasmalamao@gmail.com>
2020-09-10 22:51:40 -0300

    Busca de estados e cidades

---
+++ src/main/java/com/giga/cursomc/resources/EstadoResource.java
@@ -0,0 +1,44 @@
+package com.giga.cursomc.resources;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.giga.cursomc.domain.Cidade;
+import com.giga.cursomc.domain.Estado;
+import com.giga.cursomc.dto.CidadeDTO;
+import com.giga.cursomc.dto.EstadoDTO;
+import com.giga.cursomc.services.CidadeService;
+import com.giga.cursomc.services.EstadoService;
+
+@RestController
+@RequestMapping(value="/estados")
+public class EstadoResource {
+
+	@Autowired
+	private EstadoService service;
+	
+	@Autowired
+	private CidadeService cidadeService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<EstadoDTO>> findAllByOrderByNome() {
+		List<Estado> list = service.findAll();
+		List<EstadoDTO> listDto = list.stream().map(obj -> new EstadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{estadoId}/cidades", method=RequestMethod.GET)
+	public ResponseEntity<List<CidadeDTO>> findCidades(@PathVariable Integer estadoId) {
+		List<Cidade> list = cidadeService.findByEstado(estadoId);
+		List<CidadeDTO> listDto = list.stream().map(obj -> new CidadeDTO(obj)).collect(Collectors.toList());  
+		return ResponseEntity.ok().body(listDto);
+	}
+
+}
---
+++ src/main/java/com/giga/cursomc/services/CidadeService.java
@@ -0,0 +1,21 @@
+package com.giga.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.giga.cursomc.domain.Cidade;
+import com.giga.cursomc.respositories.CidadeRepository;
+
+@Service
+public class CidadeService {
+
+	@Autowired
+	private CidadeRepository repo;
+	
+	public List<Cidade> findByEstado(Integer estadoId){
+		return repo.findCidades(estadoId);
+	}
+	
+}
